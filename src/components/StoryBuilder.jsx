53c6ed70e20f500796c2f8a961360f1379855c28
lauragift21 <laurandidi21@gmail.com>
2023-11-16 12:20:33 +0100

    add completed story

--- src/components/StoryBuilder.jsx
+++ src/components/StoryBuilder.jsx
@@ -2,54 +2,69 @@ import React, { useState } from "react";
 import styles from './Storybuilder.module.css';
 
 const StoryBuilder = ({ story }) => {
-  const [inputValues, setInputValues] = useState({});
-  const [completedStory, setCompletedStory] = useState('');
   const placeholders = story.content.match(/\[.*?\]/g) || [];
-
-  const handleInputChange = (index, value) => {
-    setInputValues({ ...inputValues, [index]: value });
-  };
+  const [inputValues, setInputValues] = useState(new Array(placeholders.length).fill(''));
+  const [completedStory, setCompletedStory] = useState('');
 
   const formStyle = {
     backgroundColor: story.color,
-  }
+  };
 
   const labelStyle = {
     color: story.color,
     borderTop: `2px solid ${story.color}`
-  }
+  };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const handleInputChange = (index, newValue) => {
+    const newInputValues = [...inputValues];
+    newInputValues[index] = newValue;
+    setInputValues(newInputValues);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
     let filledStory = story.content;
-    Object.values(inputValues).forEach(value => {
-      filledStory = filledStory.replace(/\[.*?\]/, value);
+
+    placeholders.forEach((placeholder, index) => {
+      const replacement = inputValues[index] || 'Empty Text Field';
+      filledStory = filledStory.replace(placeholder, `<span class="${styles.highlight}">${replacement}</span>`);      
     });
     setCompletedStory(filledStory);
   };
 
   return (
-      <div>
+    <div>
       <form className={styles.form} style={formStyle} onSubmit={handleSubmit}>
-        <p>Fill in the blank fields below</p>
-        <div className={styles.gridContainer}>
-          {placeholders.map((placeholder, index) => (
-            <div key={index} className={styles.inputGroup}>
-              <input
-                id={`input-${index}`}
-                type="text"
-                onChange={(e) => handleInputChange(index, e.target.value)}
-              />
-              <label className={styles.label} style={labelStyle} htmlFor={`input-${index}`}>
-                {placeholder.replace(/[\[\]]/g, '')}
-              </label>
-            </div>
-          ))}
-        </div>
+        {!completedStory && (
+          <div className={styles.gridContainer}>
+            {placeholders.map((placeholder, index) => (
+              <div key={index} className={styles.inputGroup}>
+                <input
+                  id={`input-${index}`}
+                  type="text"
+                  autoComplete="off"
+                  onChange={(e) => handleInputChange(index, e.target.value)}
+                  value={inputValues[index] || ''}
+                />
+                <label className={styles.label} style={labelStyle} htmlFor={`input-${index}`}>
+                  {placeholder.replace(/[\[\]]/g, '')}
+                </label>
+              </div>
+            ))}
+          </div>
+        )}
+        {!completedStory && (
+          <>
+            <button type="submit" className="button" style={labelStyle}>Generate Story</button>
+          </>
+        )}
+        {completedStory && (
+          <>
+            <div className={styles.completedStory} dangerouslySetInnerHTML={{ __html: completedStory }}></div>
+          </>
+        )}
       </form>
-      <a  className={styles.backButton} href="/stories">Back</a>
-      <button className="button" style={formStyle} type="submit">Generate Story</button>
-      {completedStory && <p className={styles.completedStory}>{completedStory}</p>}
+      <a className={styles.backButton} href="/stories">Back</a>
     </div>
   );
 };
